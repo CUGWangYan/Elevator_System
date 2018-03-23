90fddb10bf0b089b0e4a1e33514e3456dda3a6a0
CUGWangYan <35526180+CUGWangYan@users.noreply.github.com>
2018-03-23 22:22:44 +0800

    Update System.cpp
    加入了电梯超载的判断，添加了实时判断超过等待时间的判断，对电梯局部状态做了修正，更改了部分Bug

--- System.cpp
+++ System.cpp
@@ -1,29 +1,38 @@
 #include "System.h"
 #include <Windows.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
 time_t StartTime;//定义系统开始时间
 time_t Now;//系统当前时间
 int Wait = 0;//电梯处于等待状态的时间
 void System::Initialize() {//初始化函数
 	time(&StartTime);
 	srand((unsigned)time(NULL));//随机种子
-	static int No = 1;
-	for (int i = 0; i <5; i++, No++) {
-		int startFloor = rand() % 9 + 1;
-		int endFloor = rand() % 9 + 1;
-		int waitTime = rand() % 52;
-		time(&StartTime);
-		time_t Starttime = StartTime + rand() % 20;
-		Person per(No, startFloor, endFloor, waitTime, Starttime);
-		OutList.PushBack(per);
-	}
-	OutList.Display();
+	time(&StartTime);
 }
 bool System::StandBy() {//等候命令的函数
+	if (_kbhit()) {
+		char a=getchar();
+		getchar();
+		int n = int(a - '0');
+		cout << "生成了"<<n<<"个人" << endl;
+		static int No = 1;
+		for (int i = 0; i < n; i++, No++) {
+		int startFloor = rand() % 9 + 1;
+		int endFloor = rand() % 9 + 1;
+		int waitTime = rand() % 60 + 30;
+		time(&Now);
+		Person per(No, startFloor, endFloor, waitTime, Now);
+		OutList.PushBack(per);
+		}
+		OutList.Display();
+		}
 	BeyondToleration();//每秒钟检查是否有人等不及了
 	Sleep(1000);//设置每一秒钟检查请求一次
 	if (ele.getCondition() == Waiting) { Wait++; }
 	if (ele.getCondition() == Waiting&&OutList.IsEmpty() == true && InList.IsEmpty() == true) {
-		if (Wait > 10)
+		if (Wait > 30)
 		{
 			cout << "处于等待阶段时间大于30秒回到一楼" << endl;
 			//SetDirection(1);//因为电梯里外都没人函数没有停止条件所以不能调用
@@ -47,11 +56,18 @@ bool System::StandBy() {//等候命令的函数
 	return false;
 }
 void System::SetDirection(int floor) {
+	BeyondToleration();
 	if (floor > ele.getfloor()) {
 		ele.setCondition(Up);
 		time(&Now);
 		cout << "第" << difftime(Now, StartTime) << "秒电梯开始向上加速" << endl;
-		GoUp();
+		while (ele.getfloor() != floor) {
+			ele.GoUp();
+			Sleep(GoUpTime);
+			time(&Now);
+			cout << "第" << difftime(Now, StartTime) << "秒电梯正在上行..." << endl;
+		}
+		Arrive();
 		Sleep(Accelerate);
 		return;
 	}
@@ -59,14 +75,25 @@ void System::SetDirection(int floor) {
 		ele.setCondition(Down);
 		time(&Now);
 		cout << "第" << difftime(Now, StartTime) << "秒电梯开始向下加速" << endl;
-		GoDown();
+		while (ele.getfloor() != floor) {
+			ele.GoDown();
+			Sleep(GoDownTime);
+			time(&Now);
+			cout << "第" << difftime(Now, StartTime) << "秒电梯正在下行..." << endl;
+		}
+		Arrive();
 		Sleep(Accelerate);
 		return;
 	}
-	else if (floor == ele.getfloor() && (ele.getCondition() == Opening || ele.getCondition() == Opened || ele.getCondition() == Closing || ele.getCondition() == Waiting)) {
+	else if (floor == ele.getfloor()) {//缺了arrive（）
 		ele.setCondition(Waiting);
-		if (ele.IsFull() == false)
-			OpenDoor();
+		if (GoIn() == true && ele.IsFull() == false)
+			In();
+		if (GoOut() == true)
+			Out();
+		if (ele.IsFull() == true) {
+			cout << "电梯人满了" << endl;
+		}
 		return;
 	}
 }
@@ -93,6 +120,7 @@ bool System::GoOut() {//仅判断是个有人在当前楼层出去
 }
 void System::Out() {//真的将能出去的人在链表中删除
 	int count = 0;
+	condition temp = ele.getCondition();
 	for (int i = 0; i < InList.Size(); i++) {
 		if (InList.GetData(i + 1).getEndFloor() == ele.getfloor()) {
 			InList.Erase(i + 1);
@@ -105,11 +133,15 @@ void System::Out() {//真的将能出去的人在链表中删除
 	Sleep(InorOutTime*count);
 	time(&Now);
 	if (count != 0) cout << "第" << difftime(Now, StartTime) << "秒" << count << "名乘客正在出电梯门...." << endl;
+	ele.GoOut(count);
 	ele.setCondition(Closing);
 	ele.setCondition(Closed);
+	ele.setCondition(temp);
 }
 void System::In() {//真的将能进去的人在链表中删除
+	if (ele.IsFull() == true) return;
 	int count = 0;
+	condition temp = ele.getCondition();
 	for (int i = 0; i < OutList.Size(); i++) {
 		if (OutList.GetData(i + 1).getStartTime() > Now) continue;
 		else if (OutList.GetData(i + 1).getStartFloor() == ele.getfloor()) {
@@ -117,6 +149,10 @@ void System::In() {//真的将能进去的人在链表中删除
 			OutList.Erase(i + 1);
 			i--;
 			count++;
+			if (count == 13) {
+				cout << "电梯超载" << endl;
+				break;
+			}
 		}
 	}
 	ele.setCondition(Opening);
@@ -125,10 +161,14 @@ void System::In() {//真的将能进去的人在链表中删除
 	time(&Now);
 	ele.setCondition(Closing);
 	ele.setCondition(Closed);
+	ele.setCondition(temp);
+	ele.GoIn(count);
 	if (count != 0) cout << "第" << difftime(Now, StartTime) << "秒" << count << "名乘客正在进入电梯门...." << endl;
 }
 bool System::Arrive() {
+	BeyondToleration();
 	if (GoIn() == true || GoOut() == true) {
+		condition temp = ele.getCondition();
 		if (ele.getCondition() == Up) {
 			Sleep(UpDecelerate);
 			time(&Now);
@@ -150,54 +190,32 @@ bool System::Arrive() {
 		ele.setCondition(Closing);
 		ele.setCondition(Closed);
 		Sleep(closeDoor);
+		ele.setCondition(temp);
 		time(&Now);
 		cout << "第" << difftime(Now, StartTime) << "秒正在关门..." << endl;
 		return true;
 	}
 	else return false;
 }
-void System::GoUp() {
-	ele.setCondition(Up);
-	if (Arrive() == false)
-	{
-		ele.GoUp();
-		Sleep(GoUpTime);
-		time(&Now);
-		cout << "第" << difftime(Now, StartTime) << "秒电梯正在上行..." << endl;
-		GoUp();
-	}
-	else OpenDoor();
-}
-void System::GoDown() {
-	ele.setCondition(Down);
-	if (Arrive() == false) {
-		ele.GoDown();
-		Sleep(GoDownTime);
-		time(&Now);
-		cout << "第" << difftime(Now, StartTime) << "秒电梯正在下行..." << endl;
-		GoDown();
-	}
-	else
-		OpenDoor();
-}
-void System::OpenDoor() {
-	if (ele.getCondition() != Opening && ele.getCondition() != Opened) {
-		ele.setCondition(Opening);
-		Sleep(openDoor);
-		if (GoIn() == true && ele.IsFull() == false) {
-			In();
-		}
-		if (GoOut() == true) {
-			Out();
-		}
-		ele.setCondition(Opened);
-		ele.setCondition(Waiting);
-	}
-}
 void System::Run() {//电梯调度运行函数
 	int near0;
 	int near1;
 	int near2;
+	if (ele.IsFull() == true) {
+		int flag;
+		int min = 9;
+		int NearIn = 12;
+		if (InList.IsEmpty() != true) {
+			for (int i = 0; i < InList.Size(); i++) {
+				flag = abs(InList.GetData(i + 1).getEndFloor() - ele.getfloor());
+				if (flag <= min) {
+					min = flag;
+					NearIn = InList.GetData(i + 1).getEndFloor();
+				}
+			}
+		}
+		SetDirection(NearIn);
+	}
 	if (ele.getCondition() == Closed) {
 		if (OutList.IsEmpty() == true && InList.IsEmpty() == true)
 			ele.setCondition(Waiting);
